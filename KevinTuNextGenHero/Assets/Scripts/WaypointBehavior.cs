8ff5af0d4097aaf2bcbfe6de1c911609ef0bf55d
kvntwo <81603687+kvntwo@users.noreply.github.com>
2021-04-21 21:59:12 -0700

    enemies now move toward waypoints

--- KevinTuNextGenHero/Assets/Scripts/WaypointBehavior.cs
+++ KevinTuNextGenHero/Assets/Scripts/WaypointBehavior.cs
@@ -6,19 +6,17 @@ using UnityEngine.UI;
 public class WaypointBehavior : MonoBehaviour
 {
     private int maxWay = 1;
+    private int wayA, wayB, wayC, wayD, wayE, wayF = 0;
 
-    private int wayA = 0;
-    private int wayB = 0;
-    private int wayC = 0;
-    private int wayD = 0;
-    private int wayE = 0;
-    private int wayF = 0;
+    private GameObject WaypointA, WaypointB, WaypointC, WaypointD, WaypointE, WaypointF;
 
+    public GameObject[] waypoints;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,88 +24,121 @@ public class WaypointBehavior : MonoBehaviour
     {
         if(wayA < maxWay)
         {
-            GameObject WaypointA = Instantiate(Resources.Load("Prefabs/Waypoint_A") as GameObject);
-            Vector3 aPos;
+            WaypointA = Instantiate(Resources.Load("Prefabs/Waypoint_A") as GameObject);
 
-            //Spawn near top left of map;
+            Vector3 aPos;
             aPos.x = Random.Range(-80, -65);
             aPos.y = Random.Range(50, 75);
             aPos.z = 0;
-            WaypointA.transform.localPosition = aPos;
+            WaypointA.transform.position = aPos; 
+            waypoints[0] = WaypointA;
             wayA++;
 
         }
 
         if(wayB < maxWay)
         {
-            GameObject WaypointB = Instantiate(Resources.Load("Prefabs/Waypoint_B") as GameObject);
-            Vector3 bPos;
+            WaypointB = Instantiate(Resources.Load("Prefabs/Waypoint_B") as GameObject);
 
-            //Spawn near top left of map;
+            Vector3 bPos;
             bPos.x = Random.Range(70, 90);
             bPos.y = Random.Range(-80, -60);
             bPos.z = 0;
             WaypointB.transform.localPosition = bPos;
+            waypoints[1] = WaypointB;
             wayB++;
 
         }
 
         if(wayC < maxWay)
         {
-            GameObject WaypointC = Instantiate(Resources.Load("Prefabs/Waypoint_C") as GameObject);
-            Vector3 cPos;
+            WaypointC = Instantiate(Resources.Load("Prefabs/Waypoint_C") as GameObject);
 
-            //Spawn near top left of map;
+            Vector3 cPos;
             cPos.x = Random.Range(20, 50);
             cPos.y = Random.Range(-15, 5);
             cPos.z = 0;
             WaypointC.transform.localPosition = cPos;
+            waypoints[2] = WaypointC;
             wayC++;
 
         }
 
         if(wayD < maxWay)
         {
-            GameObject WaypointD = Instantiate(Resources.Load("Prefabs/Waypoint_D") as GameObject);
-            Vector3 dPos;
+            WaypointD = Instantiate(Resources.Load("Prefabs/Waypoint_D") as GameObject);
 
-            //Spawn near top left of map;
+            Vector3 dPos;
             dPos.x = Random.Range(-80, -65);
             dPos.y = Random.Range(-80, -60);
             dPos.z = 0;
             WaypointD.transform.localPosition = dPos;
+            waypoints[3] = WaypointD;
             wayD++;
 
         }
 
         if(wayE < maxWay)
         {
-            GameObject WaypointE = Instantiate(Resources.Load("Prefabs/Waypoint_E") as GameObject);
-            Vector3 ePos;
+            WaypointE = Instantiate(Resources.Load("Prefabs/Waypoint_E") as GameObject);
 
-            //Spawn near top left of map;
+            Vector3 ePos;
             ePos.x = Random.Range(70, 90);
             ePos.y = Random.Range(50, 75);
             ePos.z = 0;
             WaypointE.transform.localPosition = ePos;
+            waypoints[4] = WaypointE;
             wayE++;
 
         }
 
         if(wayF < maxWay)
         {
-            GameObject WaypointF = Instantiate(Resources.Load("Prefabs/Waypoint_F") as GameObject);
+            WaypointF = Instantiate(Resources.Load("Prefabs/Waypoint_F") as GameObject);
+            
             Vector3 fPos;
-
-            //Spawn near top left of map;
             fPos.x = Random.Range(-50, -20);
             fPos.y = Random.Range(-15, 5);
             fPos.z = 0;
             WaypointF.transform.localPosition = fPos;
+            waypoints[5] = WaypointF;
             wayF++;
 
         }
+
+        if(Input.GetKeyDown(KeyCode.H)) 
+        {
+
+            //toggles visibilty for all waypoints using for loop
+            for(int i = 0;  i < waypoints.Length; i++)
+            {
+
+                waypoints[i].GetComponent<Renderer>().enabled = !waypoints[i].GetComponent<Renderer>().enabled;
+
+            }
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+
+
+
+        }
+
     }
+
+    public void shuffle()
+    {
+        
+        for(int i = 0; i < (waypoints.Length - 1); i++)
+        {
+     
+
+        }
+
+    }
+
 
     public void checkWaypoint(GameObject gc) 
     {
